410a97281ada41c2bc0261aa6b50a3baed138baf
hjkwon <hjkwon@digitalsense.co.kr>
2020-03-05 14:20:28 +0900

    feat: add select branch menu

--- clean.cmd
+++ clean.cmd
@@ -10,8 +10,21 @@ SET REPOSITORY_REMOTES=.\.git\refs\remotes\origin
 SET REPOSITORY_HEADS_CMD=dir %REPOSITORY_HEADS% /b
 SET REPOSITORY_REMOTES_CMD=dir %REPOSITORY_REMOTES% /b
 
+echo ===========================
+echo Select CheckOut Branch
+echo 1. Master
+echo 2. Alpha
+echo 3. beta
+echo ===========================
+SET /p num=Branch Number:
 
-git checkout master
+IF %num% EQU 1 (
+    git checkout master
+) ELSE IF %num% EQU 2 (
+    git checkout alpha
+) ELSE IF %num% EQU 3 (
+    git checkout beta
+)
 
 for /F "tokens=*" %%F in ('%REPOSITORY_HEADS_CMD%') do (
     if not "%%F"=="master" if not "%%F"=="alpha" if not "%%F"=="beta" (
