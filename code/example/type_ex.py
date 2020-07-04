15153ae89d4ae142b2fb9409f73aa94af35d916b
chaltteog <khg24587@naver.com>
2020-07-04 16:30:20 +0900

    feat: add python function sample

---
+++ code/example/type_ex.py
@@ -0,0 +1,36 @@
+tInt = 0
+tStr = 'test'
+tList = []
+tTuple = ()
+tSet = {0, 1}
+# 기본적으로 빈 {}은 Dict으로 인식한다.
+tDict = {}
+tBool = True
+
+# Int Type
+# print(type(tInt))
+# Result = "<class 'int'>"
+
+# Str Type
+print(type(tStr))
+# Result = "<class 'str'>"
+
+# List Type
+print(type(tList))
+# Result = "<class 'list'>"
+
+# Tuple Type
+print(type(tTuple))
+# Result = "<class 'tuple'>"
+
+# Set Type
+print(type(tSet))
+# Result = "<class 'set'>"
+
+# Dict Type
+print(type(tDict))
+# Result = "<class 'dict'>"
+
+# Bool Type
+print(type(tBool))
+# Result = "<class 'bool'>"
