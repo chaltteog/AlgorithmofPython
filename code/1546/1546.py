55621e12e53ea162e16d04ab07b6847c1eb6a07b
KwonHyeokJin <chaltteog@kakao.com>
2020-08-26 01:15:22 +0900

    feat: study algorithm to 1546 BAEKJOON issue

---
+++ code/1546/1546.py
@@ -0,0 +1,20 @@
+'''
+프로그램명 : 3052.py
+작성자 : 권혁진
+작성일 : 2020-08-26
+설명 : 평균
+참조 : https://www.acmicpc.net/problem/1546
+'''
+cnt = (int(input()))
+numList = tuple(map(int, input().split()))
+
+r = cnt
+maxNum = max(numList)
+sumNum = 0
+
+while r > 0:
+    r -= 1
+    sumNum += numList[r] / maxNum * 100
+
+sumNum = round(sumNum / cnt, 6)
+print(sumNum)
