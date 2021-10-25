403fd6a82aa6a275f9cc23bb93aae05f223f10d1
KwonHyeokJin <chaltteog@kakao.com>
2021-10-25 23:58:11 +0900

    feat: study programmers 42576 lessons

---
+++ code/programmers/lessons_42576.py
@@ -0,0 +1,14 @@
+participant = ["mislav", "stanko", "mislav", "ana"]
+completion = ["stanko", "ana", "mislav"]
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    
+    return participant[i+1]
+
+print(solution(participant,completion))
