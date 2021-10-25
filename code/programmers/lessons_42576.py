a6c5586e0f5d5c085a9fdaaa7355d6ed39edcdcf
KwonHyeokJin <chaltteog@kakao.com>
2021-10-26 00:17:08 +0900

    feat: study programmers 42576 lessons using hash

--- code/programmers/lessons_42576.py
+++ code/programmers/lessons_42576.py
@@ -1,14 +1,34 @@
 participant = ["mislav", "stanko", "mislav", "ana"]
 completion = ["stanko", "ana", "mislav"]
 
+'''
+첫번째 문제 풀이 Hash를 미사용함
+- Hash의 관한 알고리즘이지만 Hash를 사용하지않고 풀어 버림
+'''
+# def solution(participant, completion):
+#     participant.sort()
+#     completion.sort()
+    
+#     for i in range(len(completion)):
+#         if participant[i] != completion[i]:
+#             return participant[i]
+    
+#     return participant[i+1]
+
+'''
+두번째 문제 풀이 Hash를 사용함
+'''
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
+    dic = {}
+    num = 0
     
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
-    
-    return participant[i+1]
+    for player in participant:
+        dic[hash(player)] = player
+        num += hash(player)
+
+    for complet in completion:
+        num -= hash(complet)
+
+    return dic[num]
 
 print(solution(participant,completion))
